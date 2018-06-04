60fcd4b72f017c80f50d7657808f5ef0664fd004
Roberto Perez <31894578+Rlperez1207@users.noreply.github.com>
2018-06-04 11:28:33 -0400

    buttons are now done

--- game_rules.py
+++ game_rules.py
@@ -1,19 +1,22 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
 import random
+
+class Widgets(Widget):
+    pass
 class TestApp(GridLayout):
-    def __init__(self):
-        layout = GridLayout(cols=2)
-        layout.add_widget(Button(text='Hello 1'))
-        layout.add_widget(Button(text='World 1'))
-        layout.add_widget(Button(text='Hello 2'))
-        layout.add_widget(Button(text='World 2'))
+    def __init__(self,**kwargs):
+        super(TestApp,self).__init__(**kwargs)#use multi inheritence
+        self.cols = 2
+        self.add_widget(Button(text='Green'))
+        self.add_widget(Button(text='Red'))
+        self.add_widget(Button(text='Yellow'))
+        self.add_widget(Button(text='Blue'))
 class simon(App):
     def build(self):
-        return TestApp()
-
-simon().run()
+        return Widgets()
 
 class Game:
   def __init__(self):
@@ -53,5 +56,6 @@ class Game:
         self.sequence.append(random.randint(0,3))
 
 if __name__ == "__main__":
+  simon().run()
   a = Game()
   a.play()
