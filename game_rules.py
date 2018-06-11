96357a2fd8b1b29f5c64e4ec52b4c63f5fb5546d
Roberto Perez <31894578+Rlperez1207@users.noreply.github.com>
2018-06-11 04:50:57 -0400

    i tried to remake the code but it went so bad

--- game_rules.py
+++ game_rules.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+
 from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 import random
@@ -18,37 +20,50 @@ class TestApp(GridLayout):
         self.add_widget(Button(text='Blue'))
 
 class The_game(Widget):
+  score = NumericProperty(0)
+  pick = ObjectProperty(None)
+  sequence = ObjectProperty(None)
+  z = ObjectProperty(None)
+  r = ObjectProperty(None)
+  yellow = ObjectProperty(None)
+  green = ObjectProperty(None)
+  def init(self):
+      self.state = [self.green,self.red,self.yellow,self.blue]
+      self.moves = []
+      self.game_over = False
+      self.r = int(1)
+      self.z = int(0)
+      self.sequence = [(random.choice(self.state))]
+
+  def check(self):
+      self.score += len([self.sequence])
+      for i in range(len([self.sequence])):
+        self.pick = 6
+  def win(self):
+      if not self.moves==self.sequence:
+        self.game_over = True
+        Label(text="You Lose")
+      if not self.game_over:
+        self.sequence.append(random.choice(self.state))
 
   def move(self):
-    self.state = ["green","red","yellow","blue"]
-    self.sequence = [(random.randint(0,3))]
-    self.moves = []
-    self.game_over = False
     while True:
-      try:
         self.moves = []
-        for i in range(len(self.sequence)):
-          pick = str(input("Pick a color dude: "))
-          self.moves.append(int(pick))
-        if self.moves[-1] not in range(4):
-          raise ValueError
-        break
-      except ValueError:
-        print("I dont know what you did but its wrong")
-        print(self)
+        if self.pick in range(0,3):
+            self.check()
+            self.moves.append(int(self.pick))
+            self.pick = 6
+        if self.pick == 6:
+            for i in range(len(self.sequence)):
+                self.sequence[0] = 0,0,0,1
   def play(self):
     self.game_over = False
     while not self.game_over:
       self.move()
-      if not self.moves==self.sequence:
-        self.game_over = True
-        print("YOU LOSE")
-      if not self.game_over:
-        self.sequence.append(random.randint(0,3))
+      self.win()
 class simon(App):
     def build(self):
         re= The_game()
-        re.play()
         return re
 
 if __name__ == "__main__":
